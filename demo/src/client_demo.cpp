2ec332f2118e305153fdcc394ff794533572a2da
Mike Ranzinger <Mike@alien.(none)>
2014-02-20 00:39:23 -0700

    Adding windows port

--- demo/src/client_demo.cpp
+++ demo/src/client_demo.cpp
@@ -1,7 +1,7 @@
 /*
  * File description: demo.cpp
- * Author information: Mike Ranzinger mranzinger@alchemyapi.com
- * Copyright information: Copyright Orchestr8 LLC
+ * Author information: Mike Raninger mikeranzinger@gmail.com
+ * Copyright information: Copyright Mike Ranzinger
  */
 
 #include <iostream>
