120aef1c5286690da092bef6c3ef41ab3da732f1
Mike <mikeranzinger@gmail.com>
2014-12-12 12:01:22 -0700

    Adding a proxy server tool

---
+++ tools/proxy_server/proxy_server.cpp
@@ -0,0 +1,17 @@
+/*
+ * proxy_server.cpp
+ *
+ *  Created on: Dec 12, 2014
+ *      Author: mranzinger
+ */
+
+#include <boost/program_options.hpp>
+
+#include <communication/messaging/axon_proxy_server.h>
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
+
+
