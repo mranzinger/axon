89fd3067c72bcc0c1fdfb026e5d52192d9dda74d
Mike <mikeranzinger@gmail.com>
2014-12-12 17:00:04 -0700

    Adding a proxy client tool that can remotely add/remove clients from an endpoint

---
+++ tools/proxy_client/proxy_client.cpp
@@ -0,0 +1,16 @@
+/*
+ * proxy_client.cpp
+ *
+ *  Created on: Dec 12, 2014
+ *      Author: mranzinger
+ */
+
+#include <functional>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
+
+
